2579cc6cc0c52310c04ef27f219de321bf9ecc9c
JDB <legodude17@users.noreply.github.com>
2020-10-25 02:31:27 -0700

    Initial version

---
+++ cli.js
@@ -0,0 +1,343 @@
+#! /usr/bin/env node
+
+/* eslint-disable no-console */
+
+const enquirer = require('enquirer');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const ll = require('listr-log');
+const {
+  promises: fs, constants: CONSTANTS, readFileSync, accessSync
+} = require('fs');
+const path = require('path');
+const tools = require('.');
+
+const { HOME } = process.env;
+
+function home([...ps]) {
+  return path.join(HOME, ...ps);
+}
+
+const GLOABLCONFIGFILE = home`.rwmodtools.json`;
+
+const RWBYPLATFORM = {
+  darwin: {
+    executable: [
+      home`Library/Application Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Contents/MacOS/RimWorld By Ludeon Studios`
+    ],
+    config: [
+      home`Library/Application Support/RimWorld/Config`
+    ],
+    mods: [
+      home`Library/Application Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods`
+    ]
+  }
+};
+
+async function exists(p) {
+  let exists;
+  // eslint-disable-next-line no-bitwise
+  try { await fs.access(p, CONSTANTS.W_OK | CONSTANTS.R_OK); exists = true; } catch (e) {
+    if (e.errno === -2) exists = false; else console.error(e);
+  }
+  return exists;
+}
+
+function existsSync(p) {
+  let exists;
+  // eslint-disable-next-line no-bitwise
+  try { accessSync(p, CONSTANTS.W_OK | CONSTANTS.R_OK); exists = true; } catch (e) {
+    if (e.errno === -2) exists = false; else console.error(e);
+  }
+  return exists;
+}
+
+// eslint-disable-next-line no-bitwise
+async function check(p, mode = CONSTANTS.W_OK | CONSTANTS.R_OK) {
+  await fs.access(p, mode);
+  return p;
+}
+
+let globalConfig;
+
+try { globalConfig = JSON.parse(readFileSync(GLOABLCONFIGFILE, 'utf8')); } catch (e) {
+  if (e.errno === -2) globalConfig = {}; else console.error(e);
+}
+
+const arger = yargs(hideBin(process.argv))
+  .command('build', 'copy all the relevant files to the rimworld mod folder')
+  .command('configure', 'configure the tool', {
+    global: {
+      alias: 'g',
+      default: false,
+      type: 'boolean',
+      describe: 'run global configuration?'
+    },
+    interactive: {
+      alias: 'i',
+      default: true,
+      type: 'boolean',
+      describe: 'run in interactive mode?'
+    },
+    auto: {
+      alias: 'a',
+      default: true,
+      type: 'boolean',
+      describe: 'attempt to automatically determine paths?'
+    },
+    keys: {
+      alias: 'k',
+      default: 'all',
+      type: 'array',
+      describe: 'which keys to change?'
+    },
+    yes: {
+      alias: 'y',
+      default: false,
+      type: 'boolean',
+      descirbe: 'write without asking first?'
+    }
+  })
+  .command('run', 'copy files, then run rimworld', yargs => yargs.option('quicktest', {
+    describe: 'pass -quicktest to RimWorld?',
+    default: false,
+    type: 'boolean'
+  }).alias('quicktest', ['q', 'quick', 'test']))
+  .config('config')
+  .alias('config', 'c')
+  .describe('config', 'path to configuration folder')
+  .default('config', '.modtools.json')
+  .epilog(`Additionally, configuraton will be loaded from ${GLOABLCONFIGFILE}`)
+  .option('rwpath', {
+    describe: 'path to rimworld executable',
+    type: 'string'
+  })
+  .alias('rwpath', ['rimworldpath', 'rimworld-path', 'path'])
+  .option('mods', {
+    describe: 'path to rimworld mods folder',
+    type: 'string'
+  })
+  .alias('mods', ['mod-folder', 'folder', 'modfolder'])
+  .option('csharp', {
+    describe: 'does this project use C#?',
+    default: true,
+    type: 'boolean'
+  })
+  .alias('csharp', ['code', 'C#'])
+  .option('folders', {
+    describe: 'override the default folders with your own list',
+    type: 'array'
+  })
+  .option('cwd', {
+    describe: 'override the cwd to work in a different one',
+    type: 'string'
+  })
+  .option('logserver', {
+    describe: 'start a server for DevHelper? (only enable if you have DevHelper installed)',
+    type: 'boolean',
+    default: true
+  })
+  .option('rwconfig', {
+    describe: 'the folder containing rimworld config',
+    type: 'string'
+  })
+  .option('port', {
+    describe: 'port of listen to for the log server',
+    type: 'number',
+    default: 8888
+  })
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .config(globalConfig);
+
+(async function cli(arger) {
+  const args = arger.argv;
+  const command = args._[0];
+  if (!command) {
+    console.error('Need a command');
+    arger.showHelp('error');
+    return;
+  }
+  if (!['run', 'configure', 'build'].includes(command)) {
+    console.error('Invalid command:', command);
+    arger.showHelp('error');
+  }
+  if (command === 'configure') {
+    const configFile = args.global ? GLOABLCONFIGFILE : args.config;
+    const configPath = path.resolve(configFile);
+
+    let config;
+    if (await exists(configPath)) {
+      const configContents = await fs.readFile(configPath, 'utf8');
+      config = JSON.parse(configContents);
+    } else {
+      config = {};
+    }
+    const oldConfig = {};
+    Object.assign(oldConfig, config);
+    const { defaulted, aliases } = arger.parsed;
+    defaulted._ = true;
+    defaulted.$0 = true;
+    defaulted.y = true;
+    defaulted.yes = true;
+    defaulted.keys = true;
+    defaulted.k = true;
+    for (const key of Object.keys(defaulted)) {
+      if (aliases[key]) {
+        aliases[key].forEach(alias => { defaulted[alias] = true; });
+      }
+    }
+    console.log(args, defaulted);
+    for (const key of Object.keys(args)) {
+      if (!defaulted[key]) config[key] = args[key];
+    }
+    if (args.auto) {
+      if (!args.global) console.error('It is recommended you only use automatic mode on global config');
+      ll.start();
+      ll.addTask({ title: 'Attempting to find RimWorld folders', name: 'find' });
+      const rw = RWBYPLATFORM[process.platform];
+      if (!rw) {
+        ll.find.error(`No information for platform ${process.platform}`);
+      } else {
+        if (!config.rwpath) {
+          const exec = rw.executable;
+          ll.find.addTask({ title: 'Finding RimWorld executable', name: 'exec' });
+          let res;
+          try {
+            res = await Promise.any(exec.map(p => check(p, CONSTANTS.X_OK)));
+          } catch (e) { res = null; }
+          if (res) {
+            config.rwpath = res;
+            ll.find.exec.complete(`Found executable at ${path.relative(process.env.HOME, res)}`);
+          } else {
+            ll.find.exec.error('Failed to find RimWorld executable', false);
+          }
+        } if (!config.mods) {
+          const { mods } = rw;
+          ll.find.addTask({ title: 'Finding RimWorld mods folder', name: 'mods' });
+          let res;
+          try {
+            res = await Promise.any(mods.map(p => check(p)));
+          } catch (e) { res = null; }
+          if (res) {
+            config.mods = res;
+            ll.find.mods.complete(`Found mods folder at ${path.relative(process.env.HOME, res)}`);
+          } else {
+            ll.find.mods.error('Failed to find RimWorld mods folder', false);
+          }
+        } if (!config.rwconfig) {
+          const rwconfig = rw.config;
+          ll.find.addTask({ title: 'Finding RimWorld config folder', name: 'config' });
+          let res;
+          try {
+            res = await Promise.any(rwconfig.map(p => check(p)));
+          } catch (e) { res = null; }
+          if (res) {
+            config.rwconfig = res;
+            ll.find.config.complete(`Found config folder at ${path.relative(process.env.HOME, res)}`);
+          } else {
+            ll.find.config.error('Failed to find RimWorld config folder', false);
+          }
+        }
+      }
+      await new Promise(resolve => setTimeout(resolve, 500));
+      ll.end();
+    }
+    if (args.interactive) {
+      const defaultFolders = [...(new Set([
+        'Defs',
+        'Patches',
+        'About',
+        'Sounds',
+        'Textures'
+      ].concat(config.folders || [])))];
+      if (args.csharp && !defaultFolders.includes('Assemblies')) {
+        defaultFolders.push('Assemblies');
+      }
+      const existingFolders = defaultFolders
+        .filter(p => existsSync(
+          path.resolve(args.cwd || process.cwd(),
+            p)
+        )).concat(config.folders || []);
+      const questions = [
+        {
+          name: 'rwpath',
+          type: 'input',
+          message: 'Enter the path to the RimWorld executable',
+          initial: config.rwpath
+        },
+        {
+          name: 'mods',
+          type: 'input',
+          message: 'Enter the path to the RimWorld mods folder',
+          initial: config.mods
+        },
+        {
+          name: 'rwconfig',
+          type: 'input',
+          message: 'Enter the path to the RimWorld config folder',
+          initial: config.rwconfig
+        },
+        {
+          name: 'csharp',
+          type: 'confirm',
+          message: 'Does your project use C#?',
+          initial: (config.csharp == null) ? true : config.csharp
+        },
+        {
+          name: 'folders',
+          type: 'multiselect',
+          message: 'Which folders to include?',
+          choices: defaultFolders,
+          initial: existingFolders
+        }
+      ];
+      const answers = await enquirer.prompt(questions);
+      for (const key of Object.keys(answers)) {
+        config[key] = answers[key];
+      }
+    }
+    let keysToChange = Object.keys(config);
+    if (args.keys.length > 1 || args.keys[0] !== 'all') {
+      keysToChange = args.keysToChange;
+    }
+    if (args.interactive) {
+      ({ keysToChange } = await enquirer.prompt({
+        name: 'keysToChange',
+        type: 'multiselect',
+        choices: Object.keys(config),
+        initial: keysToChange,
+        message: `Which keys should we update in ${configFile}?`
+      }));
+    }
+    for (const key of Object.keys(config)) {
+      if (!keysToChange.includes(key)) config[key] = oldConfig[key];
+    }
+    console.log(`To be written to ${configFile}:`);
+    console.log(JSON.stringify(config, null, 2));
+    const yes = args.yes || (await enquirer.prompt({
+      type: 'confirm',
+      message: 'Write?',
+      initial: true,
+      name: 'yes'
+    })).yes;
+    if (yes) {
+      ll.start();
+      ll.addTask({ title: 'Write to config file', name: 'write' });
+      await fs.writeFile(configPath, JSON.stringify(config, null, 2));
+      ll.write.complete(`Wrote to ${configFile}`);
+    } else {
+      console.log('Aborted');
+    }
+  }
+  args.ll = ll;
+  if (command === 'build' || command === 'run') {
+    await tools.copy(args);
+  }
+  if (command === 'run') {
+    await tools.run(args);
+  }
+  await new Promise(resolve => setTimeout(resolve, 500));
+}(arger))
+  .then(() => process.exit(0))
+  .catch(err => { console.error(err); process.exit(1); });
--- index.js
+++ index.js
@@ -1,1 +1,215 @@
-// ENTRYPOINT
+const path = require('path');
+const { promises: fs } = require('fs');
+const net = require('net');
+const figures = require('figures');
+const { Parser, Builder } = require('xml2js');
+const execa = require('execa');
+const chalk = require('chalk');
+const width = require('string-width');
+const size = require('term-size');
+const { DateTime } = require('luxon');
+
+const LEVELSTOSYMBOLS = {
+  ERROR: chalk.red(figures.cross),
+  WARN: chalk.yellow(figures.warning),
+  ASSERT: chalk.cyan('='),
+  LOG: chalk.blue(figures.info),
+  EXCEPTION: chalk.red(figures.cross)
+};
+
+module.exports = {
+  async copy({
+    mods, csharp, folders, cwd = process.cwd(), ll
+  } = {}) {
+    if (folders == null) {
+      folders = [
+        'Defs',
+        'Patches',
+        'About',
+        'Sounds',
+        'Textures'
+      ];
+      if (csharp) folders.push('Assemblies');
+    }
+    cwd = path.resolve(process.cwd(), cwd);
+    mods = path.resolve(cwd, mods);
+    const modFolderName = path.basename(cwd);
+    const modFolder = path.join(mods, modFolderName);
+    try {
+      const stat = await fs.stat(modFolder);
+      if (!stat.isDirectory()) {
+        console.error('Mod folder not a directory, deleting then recreating');
+        await fs.rm(modFolder);
+        await fs.mkdir(modFolder);
+      }
+    } catch (e) {
+      if (e.errno === -2) {
+        console.error('Mod folder not found, creating');
+        await fs.mkdir(modFolder);
+      }
+    }
+    function relativeSource(p) {
+      return path.relative(cwd, p);
+    }
+    function relativeDest(p) {
+      return path.relative(modFolder, p);
+    }
+    async function copyFolder(source, dest, ll) {
+      if (ll) ll.addTask({ name: source, title: `Copying folder ${relativeSource(source)}` });
+      let tl;
+      if (ll) tl = ll[source];
+      let stat;
+      try { stat = await fs.stat(source); } catch (e) {
+        if (e.errno === -2) {
+          if (tl) tl.error(`Folder ${relativeSource(source)} does not exist, skipping`);
+          return;
+        }
+      }
+      if (!stat.isDirectory()) {
+        if (tl) tl.failure(`ERROR: Not a directory: ${relativeSource(source)}`);
+        return;
+      }
+      try {
+        await fs.stat(dest);
+      } catch (e) {
+        if (tl) tl.info(`Creating ${relativeDest(dest)}`);
+        await fs.mkdir(dest);
+        if (tl) tl.debug(`Created ${relativeDest(dest)}`);
+      }
+      const contents = await fs.readdir(source);
+      for (const file of contents) {
+        const fileSource = path.join(source, file);
+        const fileDest = path.join(dest, file);
+        let fileStat;
+        // eslint-disable-next-line no-await-in-loop
+        try { fileStat = await fs.stat(fileSource); } catch (e) {
+          if (e.errno === -2) {
+            if (tl) tl.error(`ERROR: ${relativeSource(fileSource)} does not exist, skipping`);
+            // eslint-disable-next-line no-continue
+            continue;
+          }
+        }
+        if (fileStat.isFile()) {
+          if (tl) tl.addTask({ title: `${relativeSource(fileSource)} -> ${relativeDest(fileDest)}`, name: fileSource });
+          // eslint-disable-next-line no-await-in-loop
+          await fs.copyFile(fileSource, fileDest);
+          if (tl) tl[fileSource].complete(`Wrote ${relativeDest(fileDest)}`);
+        } else if (fileStat.isDirectory()) {
+          if (tl) tl.info(`Found inner folder ${relativeSource(fileSource)}, entering`);
+          // eslint-disable-next-line no-await-in-loop
+          await copyFolder(fileSource, fileDest, tl);
+          if (tl) tl.debug(`Done with inner folder ${relativeSource(fileSource)}`);
+        } else if (tl) {
+          tl.warning(`Found symlink or something at ${relativeSource(fileSource)}, skipping`);
+        }
+      }
+      if (tl) tl.complete(`Finished folder ${relativeSource(source)}`);
+    }
+    ll.start();
+    for (const folder of folders) {
+      // eslint-disable-next-line no-await-in-loop
+      await copyFolder(path.join(cwd, folder), path.join(modFolder, folder), ll);
+    }
+    await new Promise(resolve => setTimeout(resolve, 500));
+  },
+  async run({
+    rwpath, logserver, rwconfig, ll, port, stream, cwd = process.cwd(), quicktest
+  } = {}) {
+    if (ll) ll.start();
+    if (logserver) {
+      if (ll) ll.addTask({ name: 'server', title: 'Creating log server' });
+      const server = net.createServer(socket => {
+        stream.write(`${figures.play} Recived connection\n`);
+        socket.on('data', data => {
+          data = data.toString();
+          const line = data.split('\n')[0];
+          const parts = line.split(' ');
+          let time;
+          let figure;
+          let message;
+          if (parts.length >= 3) {
+            [time] = parts;
+            figure = LEVELSTOSYMBOLS[parts[1]];
+            message = parts.slice(2).join(' ').trim();
+          } else if (parts.length === 2) {
+            [time] = parts;
+            figure = LEVELSTOSYMBOLS.EXCEPTION;
+            message = parts.slice(1).join(' ').trim();
+          } else {
+            time = '';
+            figure = '';
+            message = parts.join(' ');
+          }
+          time = DateTime.fromFormat(time, 'yyyyMMddHHmmss.SSS');
+          time = time.toLocaleString(DateTime.TIME_WITH_SECONDS);
+          const spacing = size().columns - (width(figure) + width(message) + 3 + width(time));
+          if (spacing <= 0) {
+            stream.write(`${figure} ${message}`);
+          } else {
+            stream.write(`${figure} ${message}${' '.repeat(spacing)}${chalk.dim(`[${time}]`)}`);
+          }
+          stream.write(data
+            .split('\n')
+            .slice(1)
+            .filter(Boolean)
+            .map(str => `${' '.repeat(width(figure))}${str}\n`)
+            .join(''));
+        });
+        socket.on('end', () => {
+          stream.write(`${figures.warning} Connection finished\n`);
+        });
+      });
+      server.listen(port, () => {
+        if (ll) ll.server.complete(`Server bound to localhost:${port}`);
+      });
+      if (ll) ll.addTask({ name: 'config', title: 'Changing DevHelper config' });
+      if (ll) ll.config.addTask({ name: 'find', title: 'Attempting to find DevHelper config' });
+      const configDir = path.resolve(cwd, rwconfig);
+      const dir = await fs.readdir(configDir);
+      let configFile;
+      for (const file of dir) {
+        if (ll) ll.config.find.debug(`Checking ${file}`);
+        if (file.includes('DevHelper')) {
+          configFile = path.join(configDir, file);
+          break;
+        }
+      }
+      if (configFile) {
+        if (ll) ll.config.find.complete(`Found DevHelper config at ${path.relative(configDir, configFile)}`);
+        if (ll) ll.config.addTask({ name: 'read', title: 'Reading DevHelper config' });
+        const configText = await fs.readFile(configFile, { encoding: 'utf8' });
+        if (ll) ll.config.read.complete('Read config xml');
+        if (ll) ll.config.addTask({ name: 'parse', title: 'Parsing and changing DevHelper config' });
+        const obj = await (new Parser().parseStringPromise(configText));
+        obj.SettingsBlock.ModSettings[0].remoteLoggingEnabled = true;
+        obj.SettingsBlock.ModSettings[0].remoteLoggingHostname = 'localhost';
+        obj.SettingsBlock.ModSettings[0].remoteLoggingPort = port;
+        const newText = new Builder().buildObject(obj);
+        if (ll) ll.config.parse.complete('Parsed and generated');
+        if (ll) ll.config.addTask({ name: 'write', title: 'Writing to DevHelper config' });
+        await fs.writeFile(configFile, newText);
+        if (ll) ll.config.write.complete('Wrote new config xml');
+        if (ll) ll.config.complete('Rewrote DevHelper comfig to use our server');
+      } else {
+        if (ll) ll.config.find.error(`Could not find DevHelper config (checked ${dir.length} files)`);
+        server.close();
+      }
+    }
+    if (ll) ll.addTask({ name: 'run', title: 'Launch RimWorld' });
+    const proc = execa(rwpath, quicktest ? ['-quicktest'] : [], { cwd, all: true });
+    proc.all.on('data', data => {
+      if (data.includes('SteamAPI_Init() failed') && ll) ll.run.info('RimWorld failed to find steam API');
+      if (data.includes('Using device') && ll) {
+        ll.run.complete('RimWorld launched successfully');
+        ll.end();
+      }
+    });
+    process.on('exit', () => {
+      if (proc.exitCode != null) return;
+      stream.write(`${figures.warning} Process terminated, terminating RimWorld`);
+      proc.cancel();
+    });
+    await proc;
+    stream.write(`${figures.tick} RimWorld process completed`);
+  }
+};
